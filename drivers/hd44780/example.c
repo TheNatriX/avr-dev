f52d300f529e047621ef29a2b2e21a8a9a08568b
Cristian Ionita <cristian.ionita85@gmail.com>
2017-01-07 21:51:00 +0200

    finished standalone implementation; now it depends only on avr-libc

--- drivers/hd44780/example.c
+++ drivers/hd44780/example.c
@@ -1,4 +1,3 @@
-#define F_CPU 3686400
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -6,11 +5,24 @@
 
 void main(void)
 {
-	const char *p = "Hello World!!";
 	lcd_init(LCD_SET_TWO_LINES);
-	lcd_print(p);
+
+	/* Print on line one */
+	lcd_print("Hello World!!");
+	_delay_ms(5000);
+
+	/* Clear the display */
+	lcd_send_instr(LCD_INSTR_CLEAR_DISPLAY);
+	_delay_ms(1000);
+
+	/* Now print on two lines */
+	lcd_print("This is line one");
+	_delay_ms(2000);
+	/* Shift to line two */
+	lcd_send_instr(LCD_INSTR_SET_DDRAM | 0x40);
+	lcd_print("This is line two");
+	_delay_ms(2000);
 
 	for (;;);
 }
-
 
